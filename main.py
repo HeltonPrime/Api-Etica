5cdc12d06de4aec88455eafed157510390ee884c
HeltonPrime <helton.coder@gmail.com>
2024-09-17 23:04:27 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import sys
+import os
+
+# Obtendo o diretório do script principal (main.py)
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Caminho completo para a pasta src
+src_path = os.path.join(base_dir, 'src')
+
+# Adiciona o caminho da pasta src ao PYTHONPATH
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from src.querys import validar_usuario
-
 
 app = FastAPI()
 
@@ -13,7 +25,7 @@ class LoginRequest(BaseModel):
 # Rota para validar usuário
 @app.post("/login/")
 async def login(login: LoginRequest):
-    resultado = validar_usuario(login.usuario, login.senha)
+    resultado = validar_usuario(login.usuario, login.senha, 'src/config.ini', 'DADOS')
 
     if resultado:
         return resultado
