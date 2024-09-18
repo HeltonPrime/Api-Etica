db2309ce1e18587bad7f6d13af6faa85340f89b8
HeltonPrime <helton.coder@gmail.com>
2024-09-18 14:56:59 -0300

    Gerando Token e acessado o Dashboard
    Nessa versão, a api valida o login e gera um token.
    Com esse token já é possivel solicitar acesso ao Dashboard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import jwt
 
 # Obtendo o diretório do script principal (main.py)
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -11,26 +12,71 @@ src_path = os.path.join(base_dir, 'src')
 if src_path not in sys.path:
     sys.path.append(src_path)
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import datetime, timedelta, timezone
 from pydantic import BaseModel
-from src.querys import validar_usuario
+from querys import validar_usuario
+
+# Configurações para JWT
+SECRET_KEY = "chave_de_segurança"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 app = FastAPI()
 
-# Modelo de requisição para login
-class LoginRequest(BaseModel):
-    usuario: str
-    senha: str
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+# Criar o Token JWT
+def criar_token_jwt(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 # Rota para validar usuário
 @app.post("/login/")
-async def login(login: LoginRequest):
-    resultado = validar_usuario(login.usuario, login.senha, 'src/config.ini', 'DADOS')
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    usuario_validado = validar_usuario(form_data.username, form_data.password, 'src/config.ini', 'DADOS')
 
-    if resultado:
-        return resultado
-    else:
-        raise HTTPException(status_code=401, detail="Usuário ou senha inválidos")
+    if usuario_validado is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Usuário ou senha inválidos",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # Criação do token com o ID_PERFIL e o nome do usuário
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    token_data = {
+        "sub": usuario_validado['usuario'],
+        "id_perfil": usuario_validado['id_perfil']
+    }
+    access_token = criar_token_jwt(
+        data=token_data, expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+# Rota Dashboard
+@app.get("/dashboard/")
+async def acessar_dashboard(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        usuario = payload.get("sub")
+        id_perfil = payload.get("id_perfil")
+        if usuario is None or id_perfil is None:
+            raise HTTPException(status_code=401, detail="Token Inválido")
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token Expirado")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Token Inválido")
+
+    return {"mensagem": f"Bem-vindo ao Dashboard, {usuario}."}
+    
 
 # Inicia o servidor
 if __name__ == "__main__":
--- src/querys.py
+++ src/querys.py
@@ -1,54 +1,35 @@
-from conexao import obter_conexao  # Obtendo acesso ao banco de dados
+from conexao import obter_conexao
 
+# Validação de Usuário e Senha
 def validar_usuario(usuario, senha, filename='config.ini', section='DADOS'):
-    conn = obter_conexao(filename, section)
-    cursor = conn.cursor()
+    try:
+        
+        conn = obter_conexao(filename, section)
+        cursor = conn.cursor()    
 
-    permissoes = [        
-        'Relatorio de Vendas e Comissões',
-        'Relatorios em Geral',
-        'Giro de Produtos',
-        'Registro de Entrada',
-        'Bloqueia Relatorio Caixa Diario'
-    ]
+        sql = f'''
+            SELECT u.ID_PERFIL, u.USUARIO
+            FROM USUARIOS u        
+            WHERE u.USUARIO = upper(?) AND u.SENHA = ?                
+        '''
 
-    placeholders = ', '.join(['?'] * len(permissoes))
-
-    sql = f'''
-        SELECT u.ID, u.USUARIO, u.SENHA, u.ID_PERFIL, p.permite, p.descricao
-        FROM USUARIOS u
-        LEFT JOIN usuarios_PERMISSOES p ON u.ID_PERFIL = p.ID_PERFIL
-        WHERE u.USUARIO = upper(?) AND u.SENHA = ?        
-        AND ((p.descricao is null) or (p.descricao IN ({placeholders})))
-    '''
-
-    parametros = [usuario, senha] + permissoes
+        parametros = [usuario, senha]
     
-    cursor.execute(sql, parametros)
-    resultados = cursor.fetchall()
-    cursor.close()
-    conn.close()
+        cursor.execute(sql, parametros)
+        resultado = cursor.fetchone()
+        
 
-    if resultados:
-        # Extrai os dados do primeiro resultado (usuario e id_perfil)
-        primeiro_resultado = resultados[0]
-        usuario_info = {
-            "id": primeiro_resultado[0],
-            "usuario": primeiro_resultado[1],
-            "id_perfil": primeiro_resultado[3],
-            "permissoes": []
-        }
+        return {'id_perfil': resultado[0], 'usuario': resultado[1]} if resultado else None # {'Resposta': 'Usuário ou Senha Inválido'}
 
-        # Adiciona todas as permissões à lista "permissoes"
-        for resultado in resultados:
-            usuario_info["permissoes"].append({
-                "permite": resultado[4],
-                "permissao": resultado[5]
-            })
-
-        return usuario_info
-    else:
+    except Exception as e:
+        print(f"Erro ao validar usuário: {e}")
         return None
 
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+    
 # Teste de query
-# print(validar_usuario('ADMIN', 'trigominas'))
+#print(validar_usuario('teste', '12s3'))
